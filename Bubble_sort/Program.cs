00a1e88f4cd4cb64e1700e213a02ddc73c0b512f
Streltsov_Mike <streltsovmike@yandex.ru>
2022-06-25 12:37:39 +0300

    Add bobble sort

---
+++ Bubble_sort/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] array = new int[] { 10, 30, 400, 20, 50, 70, 1 };
+var str = string.Join(" ", array);
+
+int size = 6,
+    indexMax = size,
+    indexMin = 0,
+    index2 = 1,
+    X;
+
+while (indexMax > indexMin)
+{
+    while (indexMin < indexMax)
+    {
+        if (array[indexMin] > array[index2])
+        {
+            X = array[index2];
+            array[index2] = array[indexMin];
+            array[indexMin] = X;
+            index2++;
+            indexMin++;
+        }
+        else if(array[indexMin] <= array[index2]) 
+        {
+            index2++;
+            indexMin++;
+        }
+
+        str = string.Join(" ", array);
+        Console.WriteLine(str);
+   }
+
+    indexMax = indexMax - 1;
+    indexMin = 0;
+    index2 = 1;
+}
+
+str = string.Join(" ", array);
+Console.WriteLine("Отсортированный массив : " + str);
