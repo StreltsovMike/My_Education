32cc32143e936e1839dbf7e285df0de82857dacd
Streltsov_Mike <streltsovmike@yandex.ru>
2022-06-25 10:00:42 +0300

    Add Array flip Add Sum between Max & Min

---
+++ Array_flip/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+var str = string.Join(" ", array);
+
+Console.WriteLine("Изначальный массив : " + str);
+
+int size = 6,
+    indexMax = size,
+    indexMin = 0,
+    X;
+
+while (indexMax > indexMin)
+{
+    X = array[indexMax] - array[indexMin];
+    array[indexMax] = array[indexMax] - X;
+    array[indexMin] = array[indexMin] + X;
+    indexMax = indexMax - 1;
+    indexMin++;
+}
+str = string.Join(" ", array);
+Console.Write("Перевернутый массив : " + str);
---
+++ Sum_between_ min_max/Program.cs
@@ -0,0 +1,59 @@
+﻿int[] array = new int[] { 10, 30, 400, 20, 50, 70, 1 };
+var str = string.Join(" ", array);
+
+int size = 7, index = 0, sum = 0,
+    indexMax = 0, indexMin = 0,
+    max = array[index], min = array[index];
+
+while (index < size)
+{
+    if (array[index] > max)
+    {
+        max = array[index];
+        indexMax = index;
+    }
+    if (array[index] < min)
+    {
+        min = array[index];
+        indexMin = index;
+    }
+    index++;
+}
+
+Console.WriteLine("Максимум :" + max);
+Console.WriteLine("Минимум : " + min);
+Console.WriteLine("Индекс максимума : " + indexMax);
+Console.WriteLine("Индекс минимума : " + indexMin);
+
+Console.WriteLine("Сумма значений между Экстремумами (включительно) : ");
+
+if (indexMax > indexMin)
+{
+    while (indexMax >= indexMin)
+    {
+        sum = sum + array[indexMin];
+        Console.Write(array[indexMin]);
+         if(indexMax>indexMin)
+        {
+            Console.Write(" + ");
+        }
+        indexMin++;
+        
+    }
+}
+else if (indexMax < indexMin)
+{
+    while (indexMin >= indexMax)
+    {
+        sum = sum + array[indexMax];
+        Console.Write(array[indexMax]);
+        if(indexMin>indexMax)
+        {
+            Console.Write(" + ");
+        }
+        indexMax++;
+
+    }
+}
+
+Console.Write(" =  " + sum);
